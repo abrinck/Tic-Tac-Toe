55c470ef1181705123ff80feff737d664e992df4
abrinck <brinckaustin@gmail.com>
2020-12-09 01:05:58 -0700

    created logic

--- app.js
+++ app.js
@@ -1,17 +1,119 @@
 let playerScore = 0;
 let computerScore = 0;
+let isPlayerTurn = true;
+let isGameOver = false;
+let markerCount = 0;
 
 $(document).ready(function () {
 	$('.gameCell').on('click', selectSquare);
 });
 
-function clearGameBoard() {
-	$('.gameCell').html('');
+function resetGame() {
+	$('.gameCifell').html('');
+	playerScore = 0;
+	computerScore = 0;
+	isPlayerTurn = true;
 }
 
 function selectSquare(event) {
-	console.log('this:', this);
-	console.log('event:', event);
+	if (isPlayerTurn && !isGameOver) {
+		isPlayerTurn = false;
+		if (this.innerHTML == ''){
+			this.innerHTML = 'X';
+			markerCount++;
+		} else {
+			return;
+		}
+		if (checkForWin('X')){
+			alert('YOU WON!!!');
+			isGameOver = true;
+		} else if(markerCount >= 9) {
+			isGameOver = true;
+			alert('YOU TIED!!')
+		}else {
+			computerTurn();
+		}
 
-	this.innerHTML = 'X';
+	}
+}
+
+function computerTurn() {
+	lookForWin();
+	lookForBlock();
+	lookForTrap();
+
+
+
+	while(!isPlayerTurn && !isGameOver){
+		let randomRow = Math.floor(Math.random() * 3) + 1;
+		let randomCol = Math.floor(Math.random() * 3) + 1;
+		let cellId = `#gameGrid${randomRow}-${randomCol}`;
+		if ($(cellId).html() == '') {
+			$(cellId).html('O');
+			markerCount++;
+			if(checkForWin('O')){
+				alert('YOU LOST!!!');
+				isGameOver = true;
+			} else {
+				isPlayerTurn = true;
+			}
+		}
+	}
+}
+
+function checkForWin(marker) {
+	if (checkHorizontalWin(marker) || checkVerticalWin(marker) || checkDiagonalWin(marker)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function checkVerticalWin(marker) {
+	for (let col = 1; col <= 3; col++) {
+		for (let row = 1; row <= 3; row++){
+			let cellId = `#gameGrid${row}-${col}`;
+			if ($(cellId).html() != marker) {
+				break;
+			} else if (row === 3) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+function checkDiagonalWin(marker) {
+	for (let i = 1; i <= 3; i++) {
+		let cellId = `#gameGrid${i}-${i}`;
+		if ($(cellId).html() != marker) {
+			break;
+		} else if (i === 3) {
+			return true;
+		}
+	}
+
+	for (let i = 1; i <= 3; i++) {
+		let cellId = `#gameGrid${i}-${4 - i}`;
+		if ($(cellId).html() != marker) {
+			break;
+		} else if (i === 3) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function checkHorizontalWin(marker) {
+	for (let row = 1; row <= 3; row++) {
+		for (let col = 1; col <= 3; col++){
+			let cellId = `#gameGrid${row}-${col}`;
+			if ($(cellId).html() != marker) {
+				break;
+			} else if (col === 3) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
