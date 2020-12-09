bd65d5c9e7cfd8efd68bad9d83d4cb106cdb6d21
abrinck <brinckaustin@gmail.com>
2020-12-08 17:56:07 -0700

    clear gameboard and add X's

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let playerScore = 0;
 let computerScore = 0;
 
+$(document).ready(function () {
+	$('.gameCell').on('click', selectSquare);
+});
+
 function clearGameBoard() {
 	$('.gameCell').html('');
+}
+
+function selectSquare(event) {
+	console.log('this:', this);
+	console.log('event:', event);
+
+	this.innerHTML = 'X';
 }
